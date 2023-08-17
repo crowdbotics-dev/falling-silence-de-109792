ecff3f3051a965b31d8927199bd8efa13f17f0bd
Crowdbotics <engineering@crowdbotics.com>
2023-08-17 13:51:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-silence-de-109792",
+  url: "https://falling-silence-de-109792.botics.co",
+  api: "https://falling-silence-de-109792.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
